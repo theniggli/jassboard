3f872bd1ce5aa39dafef887cf3746c3bf5894e04
unknown <nigglrog@students.zhaw.ch>
2012-10-22 15:34:37 +0200

    Addes some stuff just little testing

--- src/ch/zhaw/MyActivity.java
+++ src/ch/zhaw/MyActivity.java
@@ -2,6 +2,9 @@ package ch.zhaw;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +14,30 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+     //   Button buttonSetTeam = (Button) findViewById(R.id.button_set_team);
+     //   buttonSetTeam.setOnClickListener(new View.OnClickListener() {
+     //       public void onClick(View v) {
+                // Perform action on click
+     //       }
+     //   });
+     //   Button buttonSetPlayer = (Button) findViewById(R.id.button_set_player);
+     //   buttonSetPlayer.setOnClickListener(new View.OnClickListener() {
+     //       public void onClick(View v) {
+      //          // Perform action on click
+      //      }
+     //   });
+        //Button buttonStartGame = (Button) findViewById(R.id.button_start_game);
+        //buttonStartGame.setOnClickListener(new View.OnClickListener() {
+         //   public void onClick(View v) {
+                // Perform action on click
+        //    }
+      //  });
+
+    }
+    public void setTeam(View view) {
+          //asdf
+        setContentView(R.layout.setteam);
+
     }
 }
