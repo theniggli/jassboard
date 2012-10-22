b7c59b4ad15487c4a91fe7c2470db5adaf935149
unknown <nigglrog@students.zhaw.ch>
2012-10-22 16:20:32 +0200

    some tests with listview

--- src/ch/zhaw/MyActivity.java
+++ src/ch/zhaw/MyActivity.java
@@ -3,41 +3,45 @@ package ch.zhaw;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MyActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
-    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-     //   Button buttonSetTeam = (Button) findViewById(R.id.button_set_team);
-     //   buttonSetTeam.setOnClickListener(new View.OnClickListener() {
-     //       public void onClick(View v) {
-                // Perform action on click
-     //       }
-     //   });
-     //   Button buttonSetPlayer = (Button) findViewById(R.id.button_set_player);
-     //   buttonSetPlayer.setOnClickListener(new View.OnClickListener() {
-     //       public void onClick(View v) {
-      //          // Perform action on click
-      //      }
-     //   });
-        //Button buttonStartGame = (Button) findViewById(R.id.button_start_game);
-        //buttonStartGame.setOnClickListener(new View.OnClickListener() {
-         //   public void onClick(View v) {
-                // Perform action on click
-        //    }
-      //  });
-
     }
+
+
     public void setTeam(View view) {
-          //asdf
         setContentView(R.layout.setteam);
 
     }
+    public void startGame(View view) {
+        //setContentView(R.layout.startGame);
+    }
+    public void setPlayer(View view) {
+        setContentView(R.layout.setplayer);
+
+        ListView playerList= (ListView) findViewById(R.id.playerList);
+        // Instanciating an array list (you don't need to do this, you already have yours)
+        ArrayList<String> your_array_list = new ArrayList<String>();
+        your_array_list.add("foo");
+        your_array_list.add("bar");
+        // This is the array adapter, it takes the context of the activity as a first // parameter, the type of list view as a second parameter and your array as a third parameter
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, your_array_list);
+        playerList.setAdapter(arrayAdapter);
+    }
+
+    //layout setplayer
+    public void addPlayer(View view) {
+        //ListViewPlayers();
+
+
+    }
+
 }
