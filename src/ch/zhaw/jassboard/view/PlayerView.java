8a12d672cba702f163bb1145eec4df3f70f2f37b
nigglrog <nigglrog@students.zhaw.ch>
2012-12-03 16:40:52 +0100

    made viewable

--- src/ch/zhaw/jassboard/view/PlayerView.java
+++ src/ch/zhaw/jassboard/view/PlayerView.java
@@ -27,7 +27,7 @@ public class PlayerView extends LinearLayout {
 
         TextView nameTextView = new TextView(context);
         playerName = player.getPlayerName();
-        nameTextView.setText(ch.zhaw.R.string.blankplayername + ": " + playerName);
+        nameTextView.setText(context.getString(ch.zhaw.R.string.playername) + ": " + playerName);
         nameTextView.setTextSize(19);
         nameTextView.setTextColor(Color.GRAY);
         nameTextView.setTypeface(Typeface.SANS_SERIF);
@@ -35,29 +35,28 @@ public class PlayerView extends LinearLayout {
 
         TextView idTextView = new TextView(context);
         playerID = player.getPlayerID();
-        idTextView.setText(ch.zhaw.R.string.id + ": " + playerID);
+        idTextView.setText(context.getString(ch.zhaw.R.string.id) + ": " + playerID);
         idTextView.setTextSize(14);
         idTextView.setTextColor(Color.GRAY);
         idTextView.setTypeface(Typeface.SANS_SERIF);
         addView(idTextView, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
-        //some Player Statistics
         TextView gamesWonPercent = new TextView(context);
-        gamesWonPercent.setText(ch.zhaw.R.string.gameswon + ": " + player.getGamesWonPercent() + "(" + player.getGamesWon() + ch.zhaw.R.string.off + player.getGamesPlayed() + ")");
+        gamesWonPercent.setText(context.getString(ch.zhaw.R.string.gameswon) + ": " + player.getGamesWonPercent() + " ( " + player.getGamesWon() + " " + context.getString(ch.zhaw.R.string.off) + " " + player.getGamesPlayed() + " )");
         gamesWonPercent.setTextSize(14);
         gamesWonPercent.setTextColor(Color.GRAY);
         gamesWonPercent.setTypeface(Typeface.SANS_SERIF);
         addView(gamesWonPercent, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView roundsWonPercent = new TextView(context);
-        roundsWonPercent.setText(ch.zhaw.R.string.roundswon + ": " + player.getRoundsWonPercent() + "(" + player.getRoundsWon() + ch.zhaw.R.string.off + player.getRoundsPlayed() + ")");
+        roundsWonPercent.setText(context.getString(ch.zhaw.R.string.roundswon) + ": " + player.getRoundsWonPercent() + " ( " + player.getRoundsWon() + " " + context.getString(ch.zhaw.R.string.off) + " " + player.getRoundsPlayed() + " )");
         roundsWonPercent.setTextSize(14);
         roundsWonPercent.setTextColor(Color.GRAY);
         roundsWonPercent.setTypeface(Typeface.SANS_SERIF);
         addView(roundsWonPercent, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView pointsMadePercent = new TextView(context);
-        pointsMadePercent.setText(ch.zhaw.R.string.pointsmade + ": " + player.getPointsMadePercent() + "(" + player.getPointsMax() + ch.zhaw.R.string.off + player.getPointsMax() + ")");
+        pointsMadePercent.setText(context.getString(ch.zhaw.R.string.pointsmade) + ": " + player.getPointsMadePercent() + " ( " + player.getPointsMax() + " " + context.getString(ch.zhaw.R.string.off) + " " + player.getPointsMax() + " )");
         pointsMadePercent.setTextSize(14);
         pointsMadePercent.setTextColor(Color.GRAY);
         pointsMadePercent.setTypeface(Typeface.SANS_SERIF);
@@ -65,28 +64,28 @@ public class PlayerView extends LinearLayout {
 
         //Schieber
         TextView schieber = new TextView(context);
-        schieber.setText(ch.zhaw.R.string.schieber);
+        schieber.setText(context.getString(ch.zhaw.R.string.schieber));
         schieber.setTextSize(14);
         schieber.setTextColor(Color.BLUE);
         schieber.setTypeface(Typeface.SANS_SERIF);
         addView(schieber, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView gamesSchieberWonPercent = new TextView(context);
-        gamesSchieberWonPercent.setText(ch.zhaw.R.string.gameswon + ": " + player.getSchieberGamesWonPercent() + "(" + player.getGamesWonSchieber() + ch.zhaw.R.string.off + player.getGamesPlayedSchieber() + ")");
+        gamesSchieberWonPercent.setText(context.getString(ch.zhaw.R.string.gameswon) + ": " + player.getSchieberGamesWonPercent() + " ( " + player.getGamesWonSchieber() + " " + context.getString(ch.zhaw.R.string.off) + " " + player.getGamesPlayedSchieber() + " )");
         gamesSchieberWonPercent.setTextSize(14);
         gamesSchieberWonPercent.setTextColor(Color.GRAY);
         gamesSchieberWonPercent.setTypeface(Typeface.SANS_SERIF);
         addView(gamesSchieberWonPercent, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView roundsSchieberWonPercent = new TextView(context);
-        roundsSchieberWonPercent.setText(ch.zhaw.R.string.roundswon + ": " + player.getSchieberRoundsWonPercent() + "(" + player.getRoundsWonSchieber() + ch.zhaw.R.string.off + player.getRoundsPlayedSchieber() + ")");
+        roundsSchieberWonPercent.setText(context.getString(ch.zhaw.R.string.roundswon) + ": " + player.getSchieberRoundsWonPercent() + " ( " + player.getRoundsWonSchieber() + " " + context.getString(ch.zhaw.R.string.off) + " " + player.getRoundsPlayedSchieber() + " )");
         roundsSchieberWonPercent.setTextSize(14);
         roundsSchieberWonPercent.setTextColor(Color.GRAY);
         roundsSchieberWonPercent.setTypeface(Typeface.SANS_SERIF);
         addView(roundsSchieberWonPercent, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView pointsSchieberMadePercent = new TextView(context);
-        pointsSchieberMadePercent.setText(ch.zhaw.R.string.pointsmade + ": " + player.getSchieberPointsMadePercent() + "(" + player.getPointsMadeSchieber() + ch.zhaw.R.string.off + player.getPointsMaxSchieber() + ")");
+        pointsSchieberMadePercent.setText(context.getString(ch.zhaw.R.string.pointsmade) + ": " + player.getSchieberPointsMadePercent() + " ( " + player.getPointsMadeSchieber() + " " + context.getString(ch.zhaw.R.string.off) + " " + player.getPointsMaxSchieber() + " )");
         pointsSchieberMadePercent.setTextSize(14);
         pointsSchieberMadePercent.setTextColor(Color.GRAY);
         pointsSchieberMadePercent.setTypeface(Typeface.SANS_SERIF);
@@ -94,28 +93,28 @@ public class PlayerView extends LinearLayout {
 
         //Coiffeur
         TextView coiffeur = new TextView(context);
-        coiffeur.setText(ch.zhaw.R.string.coiffeur);
+        coiffeur.setText(context.getString(ch.zhaw.R.string.coiffeur));
         coiffeur.setTextSize(14);
         coiffeur.setTextColor(Color.BLUE);
         coiffeur.setTypeface(Typeface.SANS_SERIF);
         addView(coiffeur, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView gamesCoiffeurWonPercent = new TextView(context);
-        gamesCoiffeurWonPercent.setText(ch.zhaw.R.string.gameswon + ": " + player.getCoiffeurGamesWonPercent() + "(" + player.getGamesWonCoiffeur() + ch.zhaw.R.string.off + player.getGamesPlayedCoiffeur() + ")");
+        gamesCoiffeurWonPercent.setText(context.getString(ch.zhaw.R.string.gameswon) + ": " + player.getCoiffeurGamesWonPercent() + " ( " + player.getGamesWonCoiffeur() + " " + context.getString(ch.zhaw.R.string.off) + " " + player.getGamesPlayedCoiffeur() + " )");
         gamesCoiffeurWonPercent.setTextSize(14);
         gamesCoiffeurWonPercent.setTextColor(Color.GRAY);
         gamesCoiffeurWonPercent.setTypeface(Typeface.SANS_SERIF);
         addView(gamesCoiffeurWonPercent, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView roundsCoiffeurWonPercent = new TextView(context);
-        roundsCoiffeurWonPercent.setText(ch.zhaw.R.string.roundswon + ": " + player.getCoiffeurRoundsWonPercent() + "(" + player.getRoundsWonCoiffeur() + ch.zhaw.R.string.off + player.getRoundsPlayedCoiffeur() + ")");
+        roundsCoiffeurWonPercent.setText(context.getString(ch.zhaw.R.string.roundswon) + ": " + player.getCoiffeurRoundsWonPercent() + " ( " + player.getRoundsWonCoiffeur() + " " + context.getString(ch.zhaw.R.string.off) + " " + player.getRoundsPlayedCoiffeur() + " )");
         roundsCoiffeurWonPercent.setTextSize(14);
         roundsCoiffeurWonPercent.setTextColor(Color.GRAY);
         roundsCoiffeurWonPercent.setTypeface(Typeface.SANS_SERIF);
         addView(roundsCoiffeurWonPercent, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView pointsCoiffeurMadePercent = new TextView(context);
-        pointsCoiffeurMadePercent.setText(ch.zhaw.R.string.pointsmade + ": " + player.getCoiffeurPointsMadePercent() + "(" + player.getPointsMadeCoiffeur() + ch.zhaw.R.string.off + player.getPointsMaxCoiffeur() + ")");
+        pointsCoiffeurMadePercent.setText(context.getString(ch.zhaw.R.string.pointsmade) + ": " + player.getCoiffeurPointsMadePercent() + " ( " + player.getPointsMadeCoiffeur() + " " + context.getString(ch.zhaw.R.string.off) + " " + player.getPointsMaxCoiffeur() + " )");
         pointsCoiffeurMadePercent.setTextSize(14);
         pointsCoiffeurMadePercent.setTextColor(Color.GRAY);
         pointsCoiffeurMadePercent.setTypeface(Typeface.SANS_SERIF);
@@ -123,39 +122,32 @@ public class PlayerView extends LinearLayout {
 
         //Differenzler
         TextView differenzler = new TextView(context);
-        differenzler.setText(ch.zhaw.R.string.differenzler);
+        differenzler.setText(context.getString(ch.zhaw.R.string.differenzler));
         differenzler.setTextSize(14);
         differenzler.setTextColor(Color.BLUE);
         differenzler.setTypeface(Typeface.SANS_SERIF);
         addView(differenzler, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView gamesDifferenzlerWonPercent = new TextView(context);
-        gamesDifferenzlerWonPercent.setText(ch.zhaw.R.string.gameswon + ": " + player.getDifferenzlerGamesWonPercent() + "(" + player.getGamesWonDifferenzler() + ch.zhaw.R.string.off + player.getGamesPlayedDifferenzler() + ")");
+        gamesDifferenzlerWonPercent.setText(context.getString(ch.zhaw.R.string.gameswon) + ": " + player.getDifferenzlerGamesWonPercent() + " ( " + player.getGamesWonDifferenzler() + " " + context.getString(ch.zhaw.R.string.off) + " " + player.getGamesPlayedDifferenzler() + " )");
         gamesDifferenzlerWonPercent.setTextSize(14);
         gamesDifferenzlerWonPercent.setTextColor(Color.GRAY);
         gamesDifferenzlerWonPercent.setTypeface(Typeface.SANS_SERIF);
         addView(gamesDifferenzlerWonPercent, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView roundsDifferenzlerWonPercent = new TextView(context);
-        roundsDifferenzlerWonPercent.setText(ch.zhaw.R.string.roundswon + ": " + player.getDifferenzlerRoundsWonPercent() + "(" + player.getRoundsWonDifferenzler() + ch.zhaw.R.string.off + player.getRoundsPlayedDifferenzler() + ")");
+        roundsDifferenzlerWonPercent.setText(context.getString(ch.zhaw.R.string.roundswon) + ": " + player.getDifferenzlerRoundsWonPercent() + " ( " + player.getRoundsWonDifferenzler() + " " + context.getString(ch.zhaw.R.string.off) + " " + player.getRoundsPlayedDifferenzler() + " )");
         roundsDifferenzlerWonPercent.setTextSize(14);
         roundsDifferenzlerWonPercent.setTextColor(Color.GRAY);
         roundsDifferenzlerWonPercent.setTypeface(Typeface.SANS_SERIF);
         addView(roundsDifferenzlerWonPercent, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView pointsDifferenzlerMadePercent = new TextView(context);
-        pointsDifferenzlerMadePercent.setText(ch.zhaw.R.string.pointsmade + ": " + player.getDifferenzlerPointsMadePercent() + "(" + player.getPointsMadeDifferenzler() + ch.zhaw.R.string.off + player.getPointsMaxDifferenzler() + ")");
+        pointsDifferenzlerMadePercent.setText(context.getString(ch.zhaw.R.string.pointsmade) + ": " + player.getDifferenzlerPointsMadePercent() + " ( " + player.getPointsMadeDifferenzler() + " " + context.getString(ch.zhaw.R.string.off) + " " + player.getPointsMaxDifferenzler() + " )");
         pointsDifferenzlerMadePercent.setTextSize(14);
         pointsDifferenzlerMadePercent.setTextColor(Color.GRAY);
         pointsDifferenzlerMadePercent.setTypeface(Typeface.SANS_SERIF);
         addView(pointsDifferenzlerMadePercent, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-
-//        player.getRoundsWonPercent();
-//        player.getPointsMadePercent();
-//        player.getSchieberGamesWonPercent();
-//        player.getSchieberRoundsWonPercent();
-//        player.getSchieberPointsMadePercent();
-
 
     }
 }
--- src/ch/zhaw/jassboard/view/TeamView.java
+++ src/ch/zhaw/jassboard/view/TeamView.java
@@ -27,15 +27,15 @@ public class TeamView extends LinearLayout {
         idTextView = new TextView(context);
 
         teamName = team.getTeamName();
-        nameTextView.setText(teamName);
+        nameTextView.setText(context.getString(ch.zhaw.R.string.teamname) + ": " + teamName);
         nameTextView.setTextSize(19);
-        nameTextView.setTextColor(Color.CYAN);
+        nameTextView.setTextColor(Color.GRAY);
         nameTextView.setTypeface(Typeface.SANS_SERIF);
 
         teamID = team.getTeamID();
-        idTextView.setText("" + teamID);
+        idTextView.setText(context.getString(ch.zhaw.R.string.id) + ": " + teamID);
         idTextView.setTextSize(14);
-        idTextView.setTextColor(Color.RED);
+        idTextView.setTextColor(Color.GRAY);
         idTextView.setTypeface(Typeface.SANS_SERIF);
 
         addView(nameTextView, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
@@ -43,21 +43,21 @@ public class TeamView extends LinearLayout {
 
         // Not Tested
         TextView gamesWonPercent = new TextView(context);
-        gamesWonPercent.setText(ch.zhaw.R.string.gameswon + ": " + team.getGamesWonPercent() + "(" + team.getGamesWon() + ch.zhaw.R.string.off + team.getGamesPlayed() + ")");
+        gamesWonPercent.setText(context.getString(ch.zhaw.R.string.gameswon) + ": " + team.getGamesWonPercent() + " ( " + team.getGamesWon() + " " + context.getString(ch.zhaw.R.string.off) + " " + team.getGamesPlayed() + " )");
         gamesWonPercent.setTextSize(14);
         gamesWonPercent.setTextColor(Color.GRAY);
         gamesWonPercent.setTypeface(Typeface.SANS_SERIF);
         addView(gamesWonPercent, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView roundsWonPercent = new TextView(context);
-        roundsWonPercent.setText(ch.zhaw.R.string.roundswon + ": " + team.getRoundsWonPercent() + "(" + team.getRoundsWon() + ch.zhaw.R.string.off + team.getRoundsPlayed() + ")");
+        roundsWonPercent.setText(context.getString(ch.zhaw.R.string.roundswon) + ": " + team.getRoundsWonPercent() + " ( " + team.getRoundsWon() + " " + context.getString(ch.zhaw.R.string.off) + " " + team.getRoundsPlayed() + " )");
         roundsWonPercent.setTextSize(14);
         roundsWonPercent.setTextColor(Color.GRAY);
         roundsWonPercent.setTypeface(Typeface.SANS_SERIF);
         addView(roundsWonPercent, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView pointsMadePercent = new TextView(context);
-        pointsMadePercent.setText(ch.zhaw.R.string.pointsmade + ": " + team.getPointsMadePercent() + "(" + team.getPointsMax() + ch.zhaw.R.string.off + team.getPointsMax() + ")");
+        pointsMadePercent.setText(context.getString(ch.zhaw.R.string.pointsmade) + ": " + team.getPointsMadePercent() + " ( " + team.getPointsMax() + " " + context.getString(ch.zhaw.R.string.off) + " " + team.getPointsMax() + " )");
         pointsMadePercent.setTextSize(14);
         pointsMadePercent.setTextColor(Color.GRAY);
         pointsMadePercent.setTypeface(Typeface.SANS_SERIF);
@@ -65,28 +65,28 @@ public class TeamView extends LinearLayout {
 
         //Schieber
         TextView schieber = new TextView(context);
-        schieber.setText(ch.zhaw.R.string.schieber);
+        schieber.setText(context.getString(ch.zhaw.R.string.schieber));
         schieber.setTextSize(14);
         schieber.setTextColor(Color.BLUE);
         schieber.setTypeface(Typeface.SANS_SERIF);
         addView(schieber, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView gamesSchieberWonPercent = new TextView(context);
-        gamesSchieberWonPercent.setText(ch.zhaw.R.string.gameswon + ": " + team.getSchieberGamesWonPercent() + "(" + team.getGamesWonSchieber() + ch.zhaw.R.string.off + team.getGamesPlayedSchieber() + ")");
+        gamesSchieberWonPercent.setText(context.getString(ch.zhaw.R.string.gameswon) + ": " + team.getSchieberGamesWonPercent() + " ( " + team.getGamesWonSchieber() + " " + context.getString(ch.zhaw.R.string.off) + " " + team.getGamesPlayedSchieber() + " )");
         gamesSchieberWonPercent.setTextSize(14);
         gamesSchieberWonPercent.setTextColor(Color.GRAY);
         gamesSchieberWonPercent.setTypeface(Typeface.SANS_SERIF);
         addView(gamesSchieberWonPercent, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView roundsSchieberWonPercent = new TextView(context);
-        roundsSchieberWonPercent.setText(ch.zhaw.R.string.roundswon + ": " + team.getSchieberRoundsWonPercent() + "(" + team.getRoundsWonSchieber() + ch.zhaw.R.string.off + team.getRoundsPlayedSchieber() + ")");
+        roundsSchieberWonPercent.setText(context.getString(ch.zhaw.R.string.roundswon) + ": " + team.getSchieberRoundsWonPercent() + " ( " + team.getRoundsWonSchieber() + " " + context.getString(ch.zhaw.R.string.off) + " " + team.getRoundsPlayedSchieber() + " )");
         roundsSchieberWonPercent.setTextSize(14);
         roundsSchieberWonPercent.setTextColor(Color.GRAY);
         roundsSchieberWonPercent.setTypeface(Typeface.SANS_SERIF);
         addView(roundsSchieberWonPercent, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView pointsSchieberMadePercent = new TextView(context);
-        pointsSchieberMadePercent.setText(ch.zhaw.R.string.pointsmade + ": " + team.getSchieberPointsMadePercent() + "(" + team.getPointsMadeSchieber() + ch.zhaw.R.string.off + team.getPointsMaxSchieber() + ")");
+        pointsSchieberMadePercent.setText(context.getString(ch.zhaw.R.string.pointsmade) + ": " + team.getSchieberPointsMadePercent() + " ( " + team.getPointsMadeSchieber() + " " + context.getString(ch.zhaw.R.string.off) + " " + team.getPointsMaxSchieber() + " )");
         pointsSchieberMadePercent.setTextSize(14);
         pointsSchieberMadePercent.setTextColor(Color.GRAY);
         pointsSchieberMadePercent.setTypeface(Typeface.SANS_SERIF);
@@ -94,28 +94,28 @@ public class TeamView extends LinearLayout {
 
         //Coiffeur
         TextView coiffeur = new TextView(context);
-        coiffeur.setText(ch.zhaw.R.string.coiffeur);
+        coiffeur.setText(context.getString(ch.zhaw.R.string.coiffeur));
         coiffeur.setTextSize(14);
         coiffeur.setTextColor(Color.BLUE);
         coiffeur.setTypeface(Typeface.SANS_SERIF);
         addView(coiffeur, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView gamesCoiffeurWonPercent = new TextView(context);
-        gamesCoiffeurWonPercent.setText(ch.zhaw.R.string.gameswon + ": " + team.getCoiffeurGamesWonPercent() + "(" + team.getGamesWonCoiffeur() + ch.zhaw.R.string.off + team.getGamesPlayedCoiffeur() + ")");
+        gamesCoiffeurWonPercent.setText(context.getString(ch.zhaw.R.string.gameswon) + ": " + team.getCoiffeurGamesWonPercent() + " ( " + team.getGamesWonCoiffeur() + " " + context.getString(ch.zhaw.R.string.off) + " " + team.getGamesPlayedCoiffeur() + " )");
         gamesCoiffeurWonPercent.setTextSize(14);
         gamesCoiffeurWonPercent.setTextColor(Color.GRAY);
         gamesCoiffeurWonPercent.setTypeface(Typeface.SANS_SERIF);
         addView(gamesCoiffeurWonPercent, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView roundsCoiffeurWonPercent = new TextView(context);
-        roundsCoiffeurWonPercent.setText(ch.zhaw.R.string.roundswon + ": " + team.getCoiffeurRoundsWonPercent() + "(" + team.getRoundsWonCoiffeur() + ch.zhaw.R.string.off + team.getRoundsPlayedCoiffeur() + ")");
+        roundsCoiffeurWonPercent.setText(context.getString(ch.zhaw.R.string.roundswon) + ": " + team.getCoiffeurRoundsWonPercent() + " ( " + team.getRoundsWonCoiffeur() + " " + context.getString(ch.zhaw.R.string.off) + " " + team.getRoundsPlayedCoiffeur() + " )");
         roundsCoiffeurWonPercent.setTextSize(14);
         roundsCoiffeurWonPercent.setTextColor(Color.GRAY);
         roundsCoiffeurWonPercent.setTypeface(Typeface.SANS_SERIF);
         addView(roundsCoiffeurWonPercent, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView pointsCoiffeurMadePercent = new TextView(context);
-        pointsCoiffeurMadePercent.setText(ch.zhaw.R.string.pointsmade + ": " + team.getCoiffeurPointsMadePercent() + "(" + team.getPointsMadeCoiffeur() + ch.zhaw.R.string.off + team.getPointsMaxCoiffeur() + ")");
+        pointsCoiffeurMadePercent.setText(context.getString(ch.zhaw.R.string.pointsmade) + ": " + team.getCoiffeurPointsMadePercent() + " ( " + team.getPointsMadeCoiffeur() + " " + context.getString(ch.zhaw.R.string.off) + " " + team.getPointsMaxCoiffeur() + " )");
         pointsCoiffeurMadePercent.setTextSize(14);
         pointsCoiffeurMadePercent.setTextColor(Color.GRAY);
         pointsCoiffeurMadePercent.setTypeface(Typeface.SANS_SERIF);
@@ -123,28 +123,28 @@ public class TeamView extends LinearLayout {
 
         //Differenzler
         TextView differenzler = new TextView(context);
-        differenzler.setText(ch.zhaw.R.string.differenzler);
+        differenzler.setText(context.getString(ch.zhaw.R.string.differenzler));
         differenzler.setTextSize(14);
         differenzler.setTextColor(Color.BLUE);
         differenzler.setTypeface(Typeface.SANS_SERIF);
         addView(differenzler, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView gamesDifferenzlerWonPercent = new TextView(context);
-        gamesDifferenzlerWonPercent.setText(ch.zhaw.R.string.gameswon + ": " + team.getDifferenzlerGamesWonPercent() + "(" + team.getGamesWonDifferenzler() + ch.zhaw.R.string.off + team.getGamesPlayedDifferenzler() + ")");
+        gamesDifferenzlerWonPercent.setText(context.getString(ch.zhaw.R.string.gameswon) + ": " + team.getDifferenzlerGamesWonPercent() + " ( " + team.getGamesWonDifferenzler() + " " + context.getString(ch.zhaw.R.string.off) + " " + team.getGamesPlayedDifferenzler() + " )");
         gamesDifferenzlerWonPercent.setTextSize(14);
         gamesDifferenzlerWonPercent.setTextColor(Color.GRAY);
         gamesDifferenzlerWonPercent.setTypeface(Typeface.SANS_SERIF);
         addView(gamesDifferenzlerWonPercent, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView roundsDifferenzlerWonPercent = new TextView(context);
-        roundsDifferenzlerWonPercent.setText(ch.zhaw.R.string.roundswon + ": " + team.getDifferenzlerRoundsWonPercent() + "(" + team.getRoundsWonDifferenzler() + ch.zhaw.R.string.off + team.getRoundsPlayedDifferenzler() + ")");
+        roundsDifferenzlerWonPercent.setText(context.getString(ch.zhaw.R.string.roundswon) + ": " + team.getDifferenzlerRoundsWonPercent() + " ( " + team.getRoundsWonDifferenzler() + " " + context.getString(ch.zhaw.R.string.off) + " " + team.getRoundsPlayedDifferenzler() + " )");
         roundsDifferenzlerWonPercent.setTextSize(14);
         roundsDifferenzlerWonPercent.setTextColor(Color.GRAY);
         roundsDifferenzlerWonPercent.setTypeface(Typeface.SANS_SERIF);
         addView(roundsDifferenzlerWonPercent, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
         TextView pointsDifferenzlerMadePercent = new TextView(context);
-        pointsDifferenzlerMadePercent.setText(ch.zhaw.R.string.pointsmade + ": " + team.getDifferenzlerPointsMadePercent() + "(" + team.getPointsMadeDifferenzler() + ch.zhaw.R.string.off + team.getPointsMaxDifferenzler() + ")");
+        pointsDifferenzlerMadePercent.setText(context.getString(ch.zhaw.R.string.pointsmade) + ": " + team.getDifferenzlerPointsMadePercent() + " ( " + team.getPointsMadeDifferenzler() + " " + context.getString(ch.zhaw.R.string.off) + " " + team.getPointsMaxDifferenzler() + " )");
         pointsDifferenzlerMadePercent.setTextSize(14);
         pointsDifferenzlerMadePercent.setTextColor(Color.GRAY);
         pointsDifferenzlerMadePercent.setTypeface(Typeface.SANS_SERIF);
