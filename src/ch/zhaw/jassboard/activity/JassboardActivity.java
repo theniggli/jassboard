7018c6024d01aa5da8ed8b994941784b024846b0
Toni Stjepanovic <stjepton@students.zhaw.ch>
2012-12-10 13:08:12 +0100

    cleanup and add comments

--- src/ch/zhaw/jassboard/activity/JassboardActivity.java
+++ src/ch/zhaw/jassboard/activity/JassboardActivity.java
@@ -1,14 +1,12 @@
 package ch.zhaw.jassboard.activity;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import ch.zhaw.jassboard.util.DatabaseHelper;
 import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
-import junit.framework.Assert;
 
 /**
- * TODO describe class
+ * Generic Jassboard that implements the basic Activity and Orm functions
  *
  * @author <a href=mailto:stjepton@students.zhaw.ch>toni</a>
  * @since 12/1/12 8:50 AM
@@ -16,47 +14,27 @@ import junit.framework.Assert;
 
 public class JassboardActivity extends OrmLiteBaseActivity<DatabaseHelper> {
     private int activityId = -1;
-//  JasstafelActivityNavigation jasstafelActivityNavigation;
 
     public JassboardActivity() {
-        if (Build.VERSION.SDK_INT >= 11) ;
-//    for (this.jasstafelActivityNavigation = new JasstafelActionBar(this); ; this.jasstafelActivityNavigation = new JasstafelMenuActivityNavigation(this))
-//      return;
     }
 
     protected void onCreate(Bundle paramBundle) {
         super.onCreate(paramBundle);
-        if (this.activityId != -1) ;
-        for (boolean bool = true; ; bool = false) {
-            Assert.assertTrue(bool);
-//      this.jasstafelActivityNavigation.setup(this.activityId);
-            return;
-        }
     }
 
     public void onDialogClosed(int paramInt, Bundle paramBundle) {
     }
 
     public boolean onOptionsItemSelected(MenuItem paramMenuItem) {
-//    if (paramMenuItem.getItemId() == 2131230760)
-//      this.jasstafelActivityNavigation.onItemSelected(Dispatcher.coiffeurTafelId);
-//    while (true)
-//    {
-//      return super.onOptionsItemSelected(paramMenuItem);
-//      if (paramMenuItem.getItemId() == 2131230755)
-//        this.jasstafelActivityNavigation.onItemSelected(Dispatcher.schieberTafelId);
-//    }
         return true;
     }
 
     protected void onPause() {
         super.onPause();
-//    this.jasstafelActivityNavigation.onPause();
     }
 
     protected void onResume() {
         super.onResume();
-//    this.jasstafelActivityNavigation.onResume();
     }
 
     protected void setActivityId(int paramInt) {
@@ -72,8 +50,7 @@ public class JassboardActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 
     @Override
     protected void onDestroy() {
-        System.out.println("App destoryed");
-
+        System.out.println("App destroyed");
         super.onDestroy();
     }
 }
